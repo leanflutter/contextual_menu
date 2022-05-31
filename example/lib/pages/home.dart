789c77e5ac887b04227b74e015460bfe6edb464a
Joshua Matthews <josh@jmatth.com>
2022-05-31 13:28:22 -0400

    Adding visual feedback for highlight and click events to example app

--- example/lib/pages/home.dart
+++ example/lib/pages/home.dart
@@ -1,9 +1,8 @@
-import 'package:bot_toast/bot_toast.dart';
+import 'package:contextual_menu/contextual_menu.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart' hide MenuItem;
 import 'package:preference_list/preference_list.dart';
-import 'package:contextual_menu/contextual_menu.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -16,6 +15,8 @@ class _HomePageState extends State<HomePage> {
   bool _shouldReact = false;
   Offset? _position;
   Placement _placement = Placement.bottomLeft;
+  String? _highlighted;
+  String? _selected;
 
   Menu? _menu;
 
@@ -24,59 +25,119 @@ class _HomePageState extends State<HomePage> {
     super.initState();
   }
 
+  void _onClick(MenuItem item) {
+    setState(() {
+      _selected = item.label;
+    });
+  }
+
+  void _onHighlight(MenuItem item) {
+    setState(() {
+      _highlighted = item.label;
+    });
+  }
+
+  void _onLoseHighlight(_) {
+    setState(() {
+      _highlighted = null;
+    });
+  }
+
   void _handleClickPopUp() {
     _menu ??= Menu(
       items: [
         MenuItem(
           label: 'Look Up "LeanFlutter"',
+          onClick: _onClick,
+          onHighlight: _onHighlight,
+          onLoseHighlight: _onLoseHighlight,
         ),
         MenuItem(
           label: 'Search with Google',
+          onClick: _onClick,
+          onHighlight: _onHighlight,
+          onLoseHighlight: _onLoseHighlight,
         ),
         MenuItem.separator(),
         MenuItem(
           label: 'Cut',
+          onClick: _onClick,
+          onHighlight: _onHighlight,
+          onLoseHighlight: _onLoseHighlight,
         ),
         MenuItem(
           label: 'Copy',
+          onClick: _onClick,
+          onHighlight: _onHighlight,
+          onLoseHighlight: _onLoseHighlight,
         ),
         MenuItem(
           label: 'Paste',
+          onClick: _onClick,
+          onHighlight: _onHighlight,
+          onLoseHighlight: _onLoseHighlight,
           disabled: true,
         ),
         MenuItem.submenu(
           label: 'Share',
+          onClick: _onClick,
+          onHighlight: _onHighlight,
+          onLoseHighlight: _onLoseHighlight,
           submenu: Menu(
             items: [
               MenuItem(
                 label: 'Item 1',
+                onClick: _onClick,
+                onHighlight: _onHighlight,
+                onLoseHighlight: _onLoseHighlight,
               ),
               MenuItem(
                 label: 'Item 2',
+                onClick: _onClick,
+                onHighlight: _onHighlight,
+                onLoseHighlight: _onLoseHighlight,
               ),
               MenuItem.checkbox(
                 label: 'Centered Layout',
+                onClick: _onClick,
+                onHighlight: _onHighlight,
+                onLoseHighlight: _onLoseHighlight,
                 checked: false,
               ),
               MenuItem.separator(),
               MenuItem.checkbox(
                 label: 'Show Primary Side Bar',
+                onClick: _onClick,
+                onHighlight: _onHighlight,
+                onLoseHighlight: _onLoseHighlight,
                 checked: true,
               ),
               MenuItem.checkbox(
                 label: 'Show Secondary Side Bar',
+                onClick: _onClick,
+                onHighlight: _onHighlight,
+                onLoseHighlight: _onLoseHighlight,
                 checked: true,
               ),
               MenuItem.checkbox(
                 label: 'Show Status Bar',
+                onClick: _onClick,
+                onHighlight: _onHighlight,
+                onLoseHighlight: _onLoseHighlight,
                 checked: true,
               ),
               MenuItem.checkbox(
                 label: 'Show Activity Bar',
+                onClick: _onClick,
+                onHighlight: _onHighlight,
+                onLoseHighlight: _onLoseHighlight,
                 checked: true,
               ),
               MenuItem.checkbox(
                 label: 'Show Panel Bar',
+                onClick: _onClick,
+                onHighlight: _onHighlight,
+                onLoseHighlight: _onLoseHighlight,
                 checked: false,
               ),
             ],
@@ -85,33 +146,51 @@ class _HomePageState extends State<HomePage> {
         MenuItem.separator(),
         MenuItem.submenu(
           label: 'Font',
+          onClick: _onClick,
+          onHighlight: _onHighlight,
+          onLoseHighlight: _onLoseHighlight,
           submenu: Menu(
             items: [
               MenuItem.checkbox(
                 label: 'Item 1',
+                onHighlight: _onHighlight,
+                onLoseHighlight: _onLoseHighlight,
                 checked: true,
                 onClick: (menuItem) {
                   menuItem.checked = !(menuItem.checked == true);
+                  _onClick(menuItem);
                 },
               ),
               MenuItem.checkbox(
                 label: 'Item 2',
+                onHighlight: _onHighlight,
+                onLoseHighlight: _onLoseHighlight,
                 checked: false,
                 onClick: (menuItem) {
                   menuItem.checked = !(menuItem.checked == true);
+                  _onClick(menuItem);
                 },
               ),
               MenuItem.separator(),
               MenuItem(
                 label: 'Item 3',
+                onClick: _onClick,
+                onHighlight: _onHighlight,
+                onLoseHighlight: _onLoseHighlight,
                 checked: false,
               ),
               MenuItem(
                 label: 'Item 4',
+                onClick: _onClick,
+                onHighlight: _onHighlight,
+                onLoseHighlight: _onLoseHighlight,
                 checked: false,
               ),
               MenuItem(
                 label: 'Item 5',
+                onClick: _onClick,
+                onHighlight: _onHighlight,
+                onLoseHighlight: _onLoseHighlight,
                 checked: false,
               ),
             ],
@@ -119,13 +198,22 @@ class _HomePageState extends State<HomePage> {
         ),
         MenuItem.submenu(
           label: 'Speech',
+          onClick: _onClick,
+          onHighlight: _onHighlight,
+          onLoseHighlight: _onLoseHighlight,
           submenu: Menu(
             items: [
               MenuItem(
                 label: 'Item 1',
+                onClick: _onClick,
+                onHighlight: _onHighlight,
+                onLoseHighlight: _onLoseHighlight,
               ),
               MenuItem(
                 label: 'Item 2',
+                onClick: _onClick,
+                onHighlight: _onHighlight,
+                onLoseHighlight: _onLoseHighlight,
               ),
             ],
           ),
@@ -163,6 +251,21 @@ class _HomePageState extends State<HomePage> {
                 _position = null;
                 _handleClickPopUp();
               },
+            ),
+          ],
+        ),
+        PreferenceListSection(
+          title: const Text('Results'),
+          children: [
+            PreferenceListItem(
+              disabled: true,
+              title: const Text('Highlighted'),
+              accessoryView: Text(_highlighted.toString()),
+            ),
+            PreferenceListItem(
+              disabled: true,
+              title: const Text('Selected'),
+              accessoryView: Text(_selected.toString()),
             ),
           ],
         ),
